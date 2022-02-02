b06d45ad78d5cf5f90d4f7f68b979984b4d5ce9f
aichumak <aichumak.dev@gmail.com>
2022-02-02 17:53:15 +0300

    Data

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,4 +1,36 @@
 package com.example.shoppinglist.data
 
-class ShopListRepositoryImpl {
+import com.example.shoppinglist.domain.ShopItem
+import com.example.shoppinglist.domain.ShopListRepository
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+    private var autoIncrementId = 0
+
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        removeShopItem(getShopItem(shopItem.id))
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return shopList.find { it.id == shopItemId }
+            ?: throw RuntimeException("Element with id:$shopItemId not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toMutableList()
+    }
+
+    override fun removeShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
 }
